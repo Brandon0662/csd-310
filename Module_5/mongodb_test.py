dcb70670271de9b0ecee056b8fe6b0e9a90f8a0c
Brandon0662 <121989029+Brandon0662@users.noreply.github.com>
2023-02-02 23:08:21 -0600

    Delete mongodb_test.py

--- Module_5/mongodb_test.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-
-url = "mongodb+srv://admin:admin@atlascluster.ztpyedf.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(url)
-db = client.pytech
-
-print(db.list_collection_names)
